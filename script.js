5aef361150dc5cfb25f1f67a7c7c4eb110b0a15a
GuilhermeSilvaM <guimenezes125@gmail.com>
2023-06-10 18:11:08 -0300

    inicio da integração com json

--- script.js
+++ script.js
@@ -5,6 +5,39 @@ function myfunction(){
     var quantidade = Number(document.querySelector("input#quantidade").value)
     var custo = Number(document.querySelector("input#custo").value)
 
+    est.push(quantidade)
+    est.push(custo)
+
+    const jsonData = JSON.stringify(est)
+    console.log(jsonData)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     var n1 = document.querySelector("td#n1")
     var n2 = document.querySelector("td#n2")
     var n3 = document.querySelector("td#n3")
@@ -21,27 +54,23 @@ function myfunction(){
     var q4 = document.querySelector("td#q4")
 
 
-
-    est.push(produto)
-    console.log(est)
-
-    if(est.length == 1){
+    if((est.length)-1 == 1){
         n1.innerHTML = produto
         q1.innerHTML = quantidade
         c1.innerHTML = custo
     }
 
-    if(est.length == 2){
+    if((est.length)-1 == 2){
         n2.innerHTML = produto
         q2.innerHTML = quantidade
         c2.innerHTML = custo
     }
-    if(est.length == 3){
+    if((est.lengt)-1 == 3){
         n3.innerHTML = produto
         q3.innerHTML = quantidade
         c3.innerHTML = custo
     }
-    if(est.length == 4){
+    if((est.length)-1 == 4){
         n4.innerHTML = produto
         q4.innerHTML = quantidade
         c4.innerHTML = custo
